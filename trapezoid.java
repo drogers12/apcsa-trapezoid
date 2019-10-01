072e113a6ca29f72066483226679899088c651c1
David Rogers <drogers@ucvts.tec.nj.us>
2019-10-01 08:50:52 -0400

    initial commit

---
+++ trapezoid.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class trapezoid {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.print("\nEnter the top dimension: ");
+        double top = in.nextLine();
+        System.out.print("\nEnter the bottom dimension: ");
+        double bottom = in.nextline();
+        System.out.print("\nEnter the height dimension ");
+        double height = in.nextline();
+
+
+
+        in.close
+    }
+}
